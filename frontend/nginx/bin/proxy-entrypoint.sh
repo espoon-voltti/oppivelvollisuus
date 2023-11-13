df167d09f5d8abff3dd47d4ea43fd9d38a4169ce
Ilja <ilja-git@users.noreply.github.com>
2023-11-13 15:34:52 +0200

    Initial CI and frontend image

---
+++ frontend/nginx/bin/proxy-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -eu
+
+# SPDX-FileCopyrightText: 2017-2021 City of Espoo
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# shellcheck disable=SC2155
+
+set -eu
+
+if [ "${DEBUG:-false}" = "true" ]; then
+  set -x
+fi
+
+export HOST_IP="UNAVAILABLE"
+
+if [ "${SERVICE_URL:-X}" = 'X' ]; then
+  echo 'ERROR: SERVICE_URL must be a non-empty string!'
+  exit 1
+fi
+
+for template in /etc/nginx/conf.d/*.template /etc/nginx/*.template; do
+    if ! test -f "$template"; then
+      continue
+    fi
+    target=$(echo "$template" | sed -e "s/.template$//")
+
+    erb "$template" > "$target"
+done
+
+if [ "${DEBUG:-false}" = "true" ]; then
+  cat /etc/nginx/nginx.conf
+  cat /etc/nginx/conf.d/default.conf
+fi
+
+exec "$@"
