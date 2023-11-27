0aa097e736b0a69888f4d024a300b24de2a68af6
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2023-11-27 10:50:58 +0200

    fix logging

---
+++ service/src/main/kotlin/fi/espoo/oppivelvollisuus/config/SsnMasker.kt
@@ -0,0 +1,14 @@
+package fi.espoo.oppivelvollisuus.config
+
+import com.fasterxml.jackson.core.JsonStreamContext
+import net.logstash.logback.mask.ValueMasker
+
+class SsnMasker : ValueMasker {
+    override fun mask(context: JsonStreamContext?, value: Any?): Any {
+        return if (value is String) {
+            value.replace(Regex("(?<!-|[\\dA-z])(\\d{2})(\\d{2})(\\d{2})[Aa+-](\\d{3})[\\dA-z](?!-)"), "REDACTED-SSN")
+        } else {
+            value ?: "null"
+        }
+    }
+}
