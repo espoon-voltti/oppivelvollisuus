8b223f8b3ca598b6e2bb666c5c28fe9e4dc06ace
Ilja <ilja-git@users.noreply.github.com>
2023-11-30 17:32:05 +0200

    S3-downloader for service

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -6,5 +6,9 @@ set -euo pipefail
 # shellcheck disable=SC2155
 export HOST_IP=$(curl --max-time 10 --silent --fail --show-error http://169.254.169.254/latest/meta-data/local-ipv4 || printf 'UNAVAILABLE')
 
+if [ "${VOLTTI_ENV:-local}" != "local" ]; then
+  s3download "$DEPLOYMENT_BUCKET" config /config
+fi
+
 # shellcheck disable=SC2086
 exec java -cp . -server $JAVA_OPTS org.springframework.boot.loader.launch.JarLauncher "$@"
