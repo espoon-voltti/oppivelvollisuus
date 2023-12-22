86e6f0921a408697a87024625194634e5cb56ef2
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2023-12-22 13:34:57 +0200

    lint fix

--- service/src/e2eTest/kotlin/fi/espoo/oppivelvollisuus/Config.kt
+++ service/src/e2eTest/kotlin/fi/espoo/oppivelvollisuus/Config.kt
@@ -3,4 +3,4 @@ package fi.espoo.oppivelvollisuus
 val runningInDocker = System.getenv("E2E_ENV") == "docker"
 val baseUrl = if (runningInDocker) "http://frontend" else "http://localhost:9000"
 
-const val e2eDebugLogging = false
+const val E2E_DEBUG_LOGGING = false
