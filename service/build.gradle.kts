6ce55d17604a5edc8c7a15cf415d7344573ecbfb
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2024-01-10 13:30:22 +0200

    add health check

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.ws:spring-ws-security")
 
