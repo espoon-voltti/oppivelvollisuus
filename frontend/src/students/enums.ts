eaebee5227748d3496c31ed6f662b0b70f0e89fa
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2024-05-22 12:24:49 +0300

    verify lint error in ci

--- frontend/src/students/enums.ts
+++ frontend/src/students/enums.ts
@@ -6,7 +6,7 @@ export const genders = ['FEMALE', 'MALE', 'OTHER'] as const
 
 export type Gender = (typeof genders)[number]
 
-export const genderNames: Record<Gender, string> = {
+export const genderNames:Record<Gender, string> = {
   FEMALE: 'Tyttö',
   MALE: 'Poika',
   OTHER: 'Muu'
