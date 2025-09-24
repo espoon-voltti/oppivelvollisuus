28df8d8b5e5c9a530b88ab8c09a0dd56aaa54702
merisaar <merisrnn@gmail.com>
2025-09-24 16:07:58 +0300

    add empty values follow_up_measures to avoid violating the constraint

--- service/src/main/resources/db/migration/V026__add_follow_up_measures_options.sql
+++ service/src/main/resources/db/migration/V026__add_follow_up_measures_options.sql
@@ -8,8 +8,11 @@ CREATE TYPE follow_up_measure AS ENUM (
     'MOVE_ABROAD'
 );
 
-
 ALTER TABLE student_cases ADD COLUMN follow_up_measures follow_up_measure[];
 
+/* Set existing rows to empty to avoid violating the new constraint */
+UPDATE student_cases SET follow_up_measures = '{}'::follow_up_measure[] WHERE finished_reason = 'COMPULSORY_EDUCATION_ENDED';
+
+/* Add constraint that follow_up_measures is required if and only if finished_reason is COMPULSORY_EDUCATION_ENDED */
 ALTER TABLE student_cases ADD CONSTRAINT check_follow_up_measure_null_or_required
     CHECK ( (finished_reason = 'COMPULSORY_EDUCATION_ENDED') = (follow_up_measures IS NOT NULL) );
